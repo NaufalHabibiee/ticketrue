158bfcebc13bd5b3d19c983456cf4bb77d5de8f7
Habibie <naufal.habibie@gmail.com>
2026-09-25 15:10:07 +0700

    Clarify dashboard network event status

--- dist/web3.js
+++ dist/web3.js
@@ -1,4 +1,4 @@
-import {chainConfig,contractAddress,deployBlock} from './config.js?v=20260925-network-isolation';
+import {chainConfig,contractAddress,deployBlock} from './config.js?v=20260925-dashboard-network-status';
 import {selectors} from './abi.js';
 import {keccak256,utf8,hexToBytes,bytesToHex} from './keccak.js';
 let account='',provider;
